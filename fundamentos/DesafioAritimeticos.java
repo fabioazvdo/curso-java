faa7472caf53b5f04728abc12bafc948a404566b
Fábio Carvalho De Azevedo <fabio.carvalho@cs.unipe.edu.br>
2025-03-02 14:33:39 -0300

    Add arithmetic challenge implementation

---
+++ fundamentos/DesafioAritimeticos.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class DesafioAritimeticos {
+    public static void main(String[] args) {
+        /*int a = 2 + 3 * 4;
+        int b  = (int) Math.pow(a, 3);
+        double c  = Math.pow(a, 3);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        */
+        System.out.println("Desafio Aritimeticos");
+
+        int exp1 = (int) Math.pow((6 * (3 + 2)), 2) / (3*2);
+        int exp2 = (1 - 5) * (2 - 7) / 2;
+        int exp3 = (int) Math.pow(exp2, 2);
+        int resul1 = (int) Math.pow((exp1 - exp3) / 10, 3);
+
+        System.out.println("O resultado é: " + resul1);
+
+
+
+
+    }
+}
